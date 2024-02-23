6f7b966e5b04d0070f937ccd4738c374089ad816
MuskanAgarwal7 <160391188+MuskanAgarwal7@users.noreply.github.com>
2024-02-23 17:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.querySelector("#input")
+const para = document.querySelector(".temperature");
+const btn = document.querySelector("button");
+const cityName = document.querySelector(".city");
+const region = document.querySelector(".region");
+const country = document.querySelector(".country");
+const text = document.querySelector(".text");
+
+(async () => {
+    let URL = 'https://api.weatherapi.com/v1/current.json?key=4b21ade6922b44ce80791051242102&q=New%20delhi&aqi=no'
+    let response = await fetch(URL);
+    let data = await response.json();
+    para.innerText = data.current.temp_c;
+    text.innerText = data.current.condition.text;
+})();
+
+
+async function weatherApi(city) {
+    let URL = `https://api.weatherapi.com/v1/current.json?key=4b21ade6922b44ce80791051242102&q=${city}&aqi=no`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    para.innerText = data.current.temp_c;
+    cityName.innerText = data.location.name;
+    region.innerText = data.location.region;
+    country.innerText = data.location.country;
+    text.innerText = data.current.condition.text;
+}
+
+
+button.addEventListener("click", () => {
+    weatherApi(input.value);
+});
